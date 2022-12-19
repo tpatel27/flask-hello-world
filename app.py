cf3d1c0af29b29926f4f4c077d75d4f2ef90b9f9
tejaspatel <tpatel0279@gmail.com>
2022-12-19 15:50:46 -0600

    new api's

--- app.py
+++ app.py
@@ -1,6 +1,52 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/api/plate1')
+def hello_world():
+    return jsonify(
+        {
+            "message": "success",
+            "plate_text": "284FH8",
+            "status": True,
+        }
+    )
+
+
+@app.route('/api/plate2')
+def hello_world():
+    return jsonify(
+        {
+            "message": "success",
+            "plate_text": "MRSNOOPY",
+            "status": True,
+        }
+    )
+
+
+@app.route('/api/plate3')
+def hello_world():
+    return jsonify(
+        {
+            "message": "success",
+            "plate_text": "KL55R2473",
+            "status": True,
+        }
+    )
+
+
+@app.route('/api/plate4')
+def hello_world():
+    return jsonify(
+        {
+            "message": "success",
+            "plate_text": "A5998",
+            "status": True,
+        }
+    )
